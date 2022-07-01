45cb638d704bededa5b161102bd81678f94d8944
David Tsay <david.e.tsay@nasa.gov>
2022-07-01 16:23:22 -0700

    provide key for v-for

--- src/plugins/plot/stackedPlot/StackedPlot.vue
+++ src/plugins/plot/stackedPlot/StackedPlot.vue
@@ -36,7 +36,7 @@
     <div class="l-view-section">
         <stacked-plot-item
             v-for="object in compositionObjects"
-            :key="object.id"
+            :key="getKeyString(object)"
             class="c-plot--stacked-container"
             :child-object="object"
             :options="options"
@@ -273,6 +273,9 @@ export default {
                 exportPNG: this.exportPNG,
                 exportJPG: this.exportJPG
             };
+        },
+        getKeyString(object) {
+            return this.openmct.objects.makeKeyString(object.identifier);
         }
     }
 };
