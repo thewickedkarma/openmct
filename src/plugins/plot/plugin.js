ff822c406877989fbadd26b10c6e3670d077caa8
David Tsay <david.e.tsay@nasa.gov>
2022-07-06 16:12:38 -0700

    Revert "disable stacked plots inspector view"
    This reverts commit 23a6281c708bc015afa1b9be064451bf272e561e.

--- src/plugins/plot/plugin.js
+++ src/plugins/plot/plugin.js
@@ -69,7 +69,7 @@ export default function () {
         openmct.objectViews.addProvider(new PlotViewProvider(openmct));
 
         openmct.inspectorViews.addProvider(new PlotsInspectorViewProvider(openmct));
-        // openmct.inspectorViews.addProvider(new StackedPlotsInspectorViewProvider(openmct));
+        openmct.inspectorViews.addProvider(new StackedPlotsInspectorViewProvider(openmct));
 
         openmct.composition.addPolicy(new OverlayPlotCompositionPolicy(openmct).allow);
         openmct.composition.addPolicy(new StackedPlotCompositionPolicy(openmct).allow);
