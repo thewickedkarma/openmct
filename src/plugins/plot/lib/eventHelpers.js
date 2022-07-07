e3713e9462d0c4b2cb5f9907a2f2a61112842489
David Tsay <david.e.tsay@nasa.gov>
2022-07-07 13:52:46 -0700

    Revert "lock eventHelpers from becoming reactified"
    This reverts commit 5f3cdc086e0c90329624ab693909d8d4fe872699.

--- src/plugins/plot/lib/eventHelpers.js
+++ src/plugins/plot/lib/eventHelpers.js
@@ -21,87 +21,73 @@
  *****************************************************************************/
 /*jscs:disable disallowDanglingUnderscores */
 
-const helperFunctions = {};
+const helperFunctions = {
+    listenTo: function (object, event, callback, context) {
+        if (!this._listeningTo) {
+            this._listeningTo = [];
+        }
 
-Object.defineProperties(helperFunctions, {
-    listenTo: {
-        value: function (object, event, callback, context) {
-            if (!this._listeningTo) {
-                this._listeningTo = [];
-            }
+        const listener = {
+            object: object,
+            event: event,
+            callback: callback,
+            context: context,
+            _cb: context ? callback.bind(context) : callback
+        };
+        if (object.addEventListener) {
+            object.addEventListener(event, listener._cb);
+        } else {
+            object.on(event, listener._cb);
+        }
 
-            const listener = {
-                object: object,
-                event: event,
-                callback: callback,
-                context: context,
-                _cb: context ? callback.bind(context) : callback
-            };
-            if (object.addEventListener) {
-                object.addEventListener(event, listener._cb);
-            } else {
-                object.on(event, listener._cb);
-            }
-
-            this._listeningTo.push(listener);
-        },
-        enumerable: false,
-        configurable: false
+        this._listeningTo.push(listener);
     },
 
-    stopListening: {
-        value: function (object, event, callback, context) {
-            if (!this._listeningTo) {
-                this._listeningTo = [];
+    stopListening: function (object, event, callback, context) {
+        if (!this._listeningTo) {
+            this._listeningTo = [];
+        }
+
+        this._listeningTo.filter(function (listener) {
+            if (object && object !== listener.object) {
+                return false;
             }
 
-            this._listeningTo.filter(function (listener) {
-                if (object && object !== listener.object) {
-                    return false;
+            if (event && event !== listener.event) {
+                return false;
+            }
+
+            if (callback && callback !== listener.callback) {
+                return false;
+            }
+
+            if (context && context !== listener.context) {
+                return false;
+            }
+
+            return true;
+        })
+            .map(function (listener) {
+                if (listener.unlisten) {
+                    listener.unlisten();
+                } else if (listener.object.removeEventListener) {
+                    listener.object.removeEventListener(listener.event, listener._cb);
+                } else {
+                    listener.object.off(listener.event, listener._cb);
                 }
 
-                if (event && event !== listener.event) {
-                    return false;
-                }
-
-                if (callback && callback !== listener.callback) {
-                    return false;
-                }
-
-                if (context && context !== listener.context) {
-                    return false;
-                }
-
-                return true;
+                return listener;
             })
-                .map(function (listener) {
-                    if (listener.unlisten) {
-                        listener.unlisten();
-                    } else if (listener.object.removeEventListener) {
-                        listener.object.removeEventListener(listener.event, listener._cb);
-                    } else {
-                        listener.object.off(listener.event, listener._cb);
-                    }
-
-                    return listener;
-                })
-                .forEach(function (listener) {
-                    this._listeningTo.splice(this._listeningTo.indexOf(listener), 1);
-                }, this);
-        },
-        enumerable: false,
-        configurable: false
+            .forEach(function (listener) {
+                this._listeningTo.splice(this._listeningTo.indexOf(listener), 1);
+            }, this);
     },
 
-    extend: {
-        value: function (object) {
-            object.listenTo = helperFunctions.listenTo;
-            object.stopListening = helperFunctions.stopListening;
-        },
-        enumerable: false,
-        configurable: false
+    extend: function (object) {
+        object.listenTo = helperFunctions.listenTo;
+        object.stopListening = helperFunctions.stopListening;
     }
-});
+};
 
 export default helperFunctions;
 
